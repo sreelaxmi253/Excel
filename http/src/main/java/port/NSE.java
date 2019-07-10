52034ea54ba9cb0444a8ecc5fe76d6c76dfc70f0
sreelaxmi253 <sreelaxmi253@gmail.com>
2019-07-10 23:45:21 +0530

    first commit

---
+++ http/src/main/java/port/NSE.java
@@ -0,0 +1,63 @@
+package port;
+import com.codoid.products.exception.FilloException;
+import com.codoid.products.fillo.Connection;
+import com.codoid.products.fillo.Fillo;
+import com.codoid.products.fillo.Recordset;
+public class NSE {
+	
+
+	
+
+	
+		public static void main(String args[]) throws FilloException{
+			Fillo fillo=new Fillo();
+			Connection conn=null;
+			try {
+				conn=fillo.getConnection("C:\\Users\\User1\\Downloads\\data1.xlsx");
+				
+			}catch(FilloException e) {
+				throw new FilloException("File not found");
+				
+				}
+			String query = "Select * from data1 ";
+			Recordset rcrdset=null;
+			try {
+				rcrdset=conn.executeQuery(query);
+			
+			}catch(FilloException e) {
+				throw new FilloException("Error executing query");
+				
+			}
+			try {
+				System.out.println("Date" + "\t\t\t" + "Open" + "\t\t\t" + "High"+  "\t\t\t" +"Low"+"\t\t"+"Close"+"\t\t"+"Shares Traded");
+				while(rcrdset.next()) {
+					
+					System.out.print(rcrdset.getField("Date")+"\t\t ");
+					System.out.print(rcrdset.getField("Open")+"\t\t");
+					System.out.print(rcrdset.getField("High")+" \t\t");
+					System.out.print(rcrdset.getField("Low")+"\t\t");
+					System.out.print(rcrdset.getField("close")+"\t\t");
+					System.out.print(rcrdset.getField("shares Traded")+"\t\t");
+					System.out.println("\n");
+				}
+				
+				
+			
+			}catch(FilloException e) {
+			throw new FilloException("No records found");
+			
+			}finally {
+				rcrdset.close();
+				conn.close();
+			}
+			
+		
+					
+				
+			
+
+		
+
+	}
+	
+}
